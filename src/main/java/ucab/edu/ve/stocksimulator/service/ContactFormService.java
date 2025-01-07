1c2b06ee22603ab35f854e3b0be43a2045b87eec
Arnoldo-182 <adleypuerta@hotmail.com>
2025-01-06 23:25:11 -0400

    ???? que

--- src/main/java/ucab/edu/ve/stocksimulator/service/ContactFormService.java
+++ src/main/java/ucab/edu/ve/stocksimulator/service/ContactFormService.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class ContactFormService { //...
+public class ContactFormService { //github reacciona dios mio
     private final ContactFormRepo contactFormRepo;
     private final UserRepo userRepo;
 
